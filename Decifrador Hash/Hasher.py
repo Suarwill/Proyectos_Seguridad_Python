36761f512152792d0b2c4960f013c22ddae18e4c
William Suarez <147040637+Suarwill@users.noreply.github.com>
2024-09-13 10:14:10 -0300

    Update Hasher.py
    Mejoras del código

--- Decifrador Hash/Hasher.py
+++ Decifrador Hash/Hasher.py
@@ -1,20 +1,25 @@
 import hashlib as hl
 
-contraseñaEnHash = "dbe9787aaf4002c6662e490b3f1f7512807459b6dee2e1c2e56738e1cbbd993c"   # Contraseña codificada en hash
-archivoDiccionario = "diccionario.txt"  # Ubicacion del diccionario
+# Contraseña codificada en HASH
+contraseñaEnHash = "dbe9787aaf4002c6662e490b3f1f7512807459b6dee2e1c2e56738e1cbbd993c"
 
+# Ubicacion del diccionario
+archivoDiccionario = "diccionario.txt"
+
+# Abriendo el diccionario
 with open (archivoDiccionario, 'r') as file:
     diccionario = [line.strip() for line in file]
     contraseñaResuelta = ""
     contraseñasExploradas = 0
+    # Cada opcion vamos transformando en HASH
     for contraseña in diccionario:
         contraseñasExploradas += 1
+        # La transformamos en hash y la comparamos con nuestro objetivo HASH buscado.
         calcularHash = hl.sha256(contraseña.encode()).hexdigest()
         if calcularHash == contraseñaEnHash:
             contraseñaResuelta = contraseña
+            print("La contraseña es: ", contraseñaResuelta)
             break
-    if calcularHash == contraseñaEnHash:
-        print("La contraseña es: ", contraseñaResuelta)
-    else:
-        print("La contraseña no se encuentra en el diccionario.")
-    print("Se exploraron " + str(contraseñasExploradas)+ " contraseñas en el diccionario.")
+        else:
+            print("La contraseña no se encuentra en el diccionario.")
+    print(f"Se exploraron {contraseñasExploradas} contraseñas en el diccionario.")
